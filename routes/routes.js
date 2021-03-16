59856815f795f2727b2c16d5ce1eefbd4ca70f3e
Mateo Ferreira <matteo948@icloud.com>
2021-03-16 16:00:39 -0400

    added html routes

--- routes/routes.js
+++ routes/routes.js
@@ -1,9 +1,30 @@
+const path = require("path");
 module.exports = function(app) {
+console.log("yay")
+ 
 
 app.get("/exercise", (req, res)=>{
-res.sendFile("./public/exercise.html")
+res.sendFile(path.join(__dirname, '../public/exercise.html'));
 
 })
 
+app.get("/exercise?", (req, res)=>{
+    res.sendFile(path.join(__dirname,"../public/exercise.html"))
+   
+    })
+
+  app.get("/", (req, res)=>{
+        res.sendFile(path.join(__dirname,"../public/index.html"))
+       
+        })
+
+
+app.get("/stats", (req, res)=>{
+    res.sendFile(path.join(__dirname,"../public/stats.html"))
+   
+    })
 
 }
+
+
+
