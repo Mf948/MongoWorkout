1f48015797bb2cb9d3fe27aabea48b4840a737ba
Mateo Ferreira <matteo948@icloud.com>
2021-03-24 18:59:06 -0400

    connectiondb

--- routes/api.js
+++ routes/api.js
@@ -1,26 +1,25 @@
 
 const Workout = require("../models/Workouts.js");
 
-console.log('ummm')
-
-
 module.exports = function(app) {
 app.post("/api/workouts", ({ body }, res) => {
     console.log("here")
 
-  Workout.create(body)
+  Workout.create({})
     .then(dbExercise => {
         console.log('idddd',dbExercise)
       res.json(dbExercise);
     })
     .catch(err => {
+      console.log(err)
       res.status(400).json(err);
     });
 });
 app.put("/api/workouts/:id", (req,res)=>{
-    console.log('iddddd',req.params)
-    console.log(req.body)
-    Workout.findByIdAndUpdate( {exercise: req.body })
+    console.log('iddddd',req.params.id)
+    console.log(req.body.id )
+    const id = req.params.id
+    Workout.findByIdAndUpdate(id,{ $push: {exercises:req.body }},{ new: true } )
     .then(dbExercise => {
         console.log(dbExercise)
       res.json(dbExercise);
@@ -30,14 +29,23 @@ app.put("/api/workouts/:id", (req,res)=>{
     });
 
 });
-app.get('/api/workouts/range',(req,res) => {
-Workout.find({}, (error,data)=>{
-  if(error){
-    res.send(error);
-  }else{
-    res.send(data)
-  }
-})
+app.get('/api/workouts',(req,res) => {
+Workout.aggregate([{  $addFields: { totalDuration: { $sum: "$exercises.duration" }}  }])
+.then(dbExercise =>{
+  res.json(dbExercise);
 })
 
+})
+app.get('/api/workouts/range', 
+(req,res)=>{
+   const id = req.params.id
+Workout.aggregate([{ $addFields: { totalDuration: { $sum: "$exercises.duration" }}  }]).sort({id: -1}).limit(7)
+.then(dbExercise =>{
+  res.json(dbExercise);
+})
+
+})
+
+
+// res.json(data[data.length-1])
 }
--- server.js
+++ server.js
@@ -10,7 +10,8 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", {
+mongoose.connect(
+  process.env.MONGODB_URI || "mongodb://localhost/workout", {
   useNewUrlParser: true,
   useFindAndModify: false
 });
